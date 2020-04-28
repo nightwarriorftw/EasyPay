92bacfc20582558c309456dcaabd690f09e823db
nightwarrior-xxx <amanv1999@gmail.com>
2020-04-29 02:42:27 +0530

    Firebase integration

--- EasyPay/config/ApiKeys.js
+++ EasyPay/config/ApiKeys.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 export const firebaseConfig = {
-  apiKey: "AIzaSyAjs9BCodGi4f74X4f67u2MUcsjsgcdq08",
-  authDomain: "oya-send.firebaseapp.com",
-  databaseURL: "https://oya-send.firebaseio.com",
-  projectId: "oya-send",
-  storageBucket: "oya-send.appspot.com",
-  messagingSenderId: "976307137791",
-  appId: "1:976307137791:web:67ee05949670438c03f0ca",
-  measurementId: "G-XBT51W1WY0",
-};
+    apiKey: "AIzaSyBcGJ9XMUZ-7Jwx7WW4Q5GWGN240lZXvzg",
+    authDomain: "easypay-a648e.firebaseapp.com",
+    databaseURL: "https://easypay-a648e.firebaseio.com",
+    projectId: "easypay-a648e",
+    storageBucket: "easypay-a648e.appspot.com",
+    messagingSenderId: "157632185042",
+    appId: "1:157632185042:web:33ceef63586ba33cad630b",
+    measurementId: "G-V60WR9KK2B"
+  };
+
 
 firebase.initializeApp(firebaseConfig);
